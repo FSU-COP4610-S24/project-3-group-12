e095ac5f9ad624dfdd1fffe9f4cd83c08f215352
SEED <rb20i@fsu.edu>
2024-04-08 23:19:38 -0400

    fixed various errors and added list directory functionality

--- src/filesys.c
+++ src/filesys.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <inttypes.h>
 #include <sys/stat.h>
+#include <dirent.h> 
 
 void displayPrompt();
 char *get_input(void);
@@ -15,7 +16,7 @@ tokenlist *get_tokens(char *input);
 void free_tokens(tokenlist *tokens);
 
 struct imageStruct {
-	int fd;
+    int fd;
     uint16_t BpSect;
     uint8_t sectpClus;
     uint8_t numFATs;
@@ -26,26 +27,45 @@ struct imageStruct {
     uint32_t totalDataClus;
     uint32_t entpFAT;
     int64_t size;
-}
+};
+struct directoryEntry {
+	uint8_t name[11];
+	uint8_t attribute;
+	uint8_t res;
+	uint8_t creationTimeTenth;
+	uint16_t creationTime;
+	uint16_t creationDate;
+	uint16_t lastAccessDate;
+	uint16_t fstClusterHi;
+	uint16_t writeTime;
+	uint16_t wrtDate;
+	uint16_t fstClusterLo;
+	uint16_t dirFileSize;
+};
 
+struct directoryEntry * directoryEntries = NULL;
 
 
 int main(int argc, char *argv[]) {
+
+	char command[100];
+	int status;
+	
 	//checking for right arguments
 	if (argc != 2) {
 		printf("Argument error: ./filesys <FAT32 image file>\n");
 		return 1;
 	}
 	
-	int status;
-	status = system("sudo mount -o loop %s ./mnt", argv[1]);
+	sprintf(command, "sudo mount -o loop %s ./mnt ", argv[1]);
+	status = system(command);
 	if(status == -1) {
 		perror("mount failed");
 		return 1;
 	}
 
-    imageStruct* image = malloc(sizeof(imageStruct));
-    image->fd = open(argv[1], 0_RDWR)
+    struct imageStruct* image = malloc(sizeof(struct imageStruct));
+    image->fd = open(argv[1], O_RDWR);
     struct stat fileInfo;
     stat(argv[1], &fileInfo);
     image->size = (int64_t) fileInfo.st_size;
@@ -57,11 +77,19 @@ int main(int argc, char *argv[]) {
 		tokenlist *tokens = get_tokens(input);
 		
 		//execution
-        if(tokens[0] == "exit") {
+        if(strcmp(tokens->items[0], "exit") == 0) {
             exit;
         }
-        if(tokens[0] == "info") {
+        if(strcmp(tokens->items[0], "info") == 0) {
             printImageStruct(image);
+        }
+        if(strcmp(tokens->items[0], "ls") == 0){
+        	if(tokens->size >= 2){
+        		listDirectories(tokens->items[1]);
+        	}
+        	else{
+        		listDirectories(".");
+        	}
         }
 		
 		free(input);
@@ -72,8 +100,26 @@ int main(int argc, char *argv[]) {
 }
 
 
+void listDirectories(char * path){
+	DIR *dir;
+	struct dirent *entry;
+	dir = opendir(path);
+
+	if (dir == NULL){
+		perror("cannot open directory");
+		return;
+	}
+	
+	while ((entry = readdir(dir)) != NULL){
+		printf("%s\n", entry->d_name);
+	}
+	
+	closedir(dir);
+
+}
+
 //assumes file descriptor is set
-void initImage(*imageStruct image) {
+void initImage(struct imageStruct *image) {
     char buf0[2];
     ssize_t bytes_read0 = pread(image->fd, buf0, 2, 11);
     image->BpSect = buf0[0] + buf0[1] << 8;
@@ -98,13 +144,15 @@ void initImage(*imageStruct image) {
 
     char buf5[4];
     ssize_t bytes_read5 = pread(image->fd, buf5, 4, 44);
-    image->rootCluster = buf5[0] + (buf5[1] << 8) + (buf5[2] << 16) + (buf5[3] << 24);
+    image->rootClus = buf5[0] + (buf5[1] << 8) + (buf5[2] << 16) + (buf5[3] << 24);
 
     uint16_t totSec16;
     uint32_t totSec32;
     ssize_t bytes_read6 = pread(image->fd, &totSec16, sizeof(totSec16), 19);
     ssize_t bytes_read7 = pread(image->fd, &totSec32, sizeof(totSec32), 32);
     image->totalSec = totSec16 ? totSec16 : totSec32;
+    
+    printImageStruct(image);
 
     uint32_t totalDataSec = image->totalSec - (image->rsvSecCnt + (image->numFATs * image->secpFAT));
     image->totalDataClus = totalDataSec / image->sectpClus;
